9c07fcabb742318ba7319ffd9d91016649ef5014
hanish7878 <hanishsinghal7878@gmail.com>
2026-02-06 17:13:14 +0530

    Add explicit mail host and port

--- server/src/utils/emailService.js
+++ server/src/utils/emailService.js
@@ -13,6 +13,9 @@ const sendEmail = async (options) => {
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
